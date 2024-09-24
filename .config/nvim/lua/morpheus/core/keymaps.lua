c18aa8831a1856704f59a13442ce4fcc4a311129
ben-newell <bennewell.mail@gmail.com>
2024-09-24 17:18:58 -0500

    [add] new keymap for search and replace

--- .config/nvim/lua/morpheus/core/keymaps.lua
+++ .config/nvim/lua/morpheus/core/keymaps.lua
@@ -21,3 +21,6 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- search and replace
+keymap.set("n", "<leader>sr", ":%s/<C-r><C-w>//gc<Left><Left><Left>", { desc = "Search and replace current word" })
