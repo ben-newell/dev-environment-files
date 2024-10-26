1120782575627151b665bfc3363fdc45a42d5263
ben-newell <bennewell.mail@gmail.com>
2024-10-26 05:10:28 -0500

    [add] .wezterm.lua font

--- .wezterm.lua
+++ .wezterm.lua
@@ -19,7 +19,8 @@ config.colors = {
 	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
 }
 
-config.font = wezterm.font("JetBrainsMono Nerd Font")
+-- config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font = wezterm.font("Iosevka Nerd Font Mono", { weight = "Black", stretch = "Normal", style = "Normal" })
 config.font_size = 19
 
 config.enable_tab_bar = false
