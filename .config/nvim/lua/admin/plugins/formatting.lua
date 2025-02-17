6b7560b3927c82c26c58056331fbc5f2adeb17cf
morpheus <bennewell.mail@gmail.com>
2025-02-17 00:39:52 -0600

    [add]

--- .config/nvim/lua/admin/plugins/formatting.lua
+++ .config/nvim/lua/admin/plugins/formatting.lua
@@ -3,7 +3,7 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   config = function()
     local conform = require("conform")
-
+    vim.g.conform_enabled = true
     conform.setup({
       formatters_by_ft = {
         javascript = { "prettier" },
@@ -21,19 +21,26 @@ return {
         lua = { "stylua" },
         python = { "isort", "black" },
       },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      },
+      notify_on_error = false,
+      notify_no_formatters = false,
     })
 
     vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-      conform.format({
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      })
+      conform.format({ lsp_fallback = true, async = false, timeout_ms = 1000 })
     end, { desc = "Format file or range (in visual mode)" })
+
+    vim.keymap.set("n", "<leader>to", function()
+      vim.g.conform_enabled = not vim.g.conform_enabled
+      print("conform " .. (vim.g.conform_enabled and "enabled" or "disabled"))
+    end, { desc = "Toggle conform autoformat" })
+
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*",
+      callback = function(args)
+        if vim.g.conform_enabled then
+          conform.format({ bufnr = args.buf, lsp_fallback = true, async = false, timeout_ms = 1000 })
+        end
+      end,
+    })
   end,
 }
